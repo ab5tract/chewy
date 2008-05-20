92552588f4ead56ca7010a7858cfd80abd93c854
Live session user <ethyrnet@yahoo.com>
2008-05-20 20:13:13 +0000

    You'll want the new example too.

--- lib/chewy.example.rb
+++ lib/chewy.example.rb
@@ -29,6 +29,41 @@ chewy.add_command(
   :regex    =>  /^pastie$/,
   :is_public  =>  true
 ) { "http://pastie.caboo.se/"}
+surfchica.add_command(
+    :syntax      => 'rand',
+    :description => 'Produce a random number from 0 to 10',
+    :regex       => /^rand$/,
+    :is_public   => true
+  ) { rand(10).to_s }
+
+surfchica.add_command(
+  :syntax   => 'pastie',
+  :description => 'For now, just outputs link to pastie.caboo.se',
+  :regex    =>  /^pastie$/,
+  :is_public  =>  true
+) do |raw,parms|
+  x = raw.bot.dcc(raw.sender[:nick])
+  puts x
+  pastie = Pastis.paste "#{x}"
+  raw.bot.say(raw.bot.chan, "#{raw.sender[:nick]} has a new pastie: #{pastie.url}")
+  nil
+end
+
+# requires hacked version of 'cheat' (github.com/ab5tract/cheat)
+surfchica.add_command(
+  :syntax   => 'cheat',
+  :description => 'Chewy helps you cheat (http://cheat.errtheblog.com/)',
+  :regex    =>  /^cheat\s+.+$/,
+  :is_public  =>  true
+) {|sender,params| args = ['--returns']; args << params; Cheat.sheets(args) }
+
+# Does nothing except connect.
+surfchica.add_command(
+  :syntax => 'dcc',
+  :description => 'Establish the facts.',
+  :regex => /^dcc/,
+  :is_public => true
+){ |r,p| r.bot.dcc(r.sender[:nick]) }
 
 chewy.connect
 chewy.join
